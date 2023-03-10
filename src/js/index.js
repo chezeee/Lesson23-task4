4c7254aaefc44ec0e5353adf2be316f69e2709b1
Ilya Yalymov <chezeee@gmail.com>
2023-03-10 20:35:46 +0300

    Initial structure added

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {HTMLButtonElement} button
+ */
+const isDisabled = (button) => {
+  // your code
+};
+
+// Sample usage - do not modify
+console.log(isDisabled(document.querySelector("#button1"))); // false
+console.log(isDisabled(document.querySelector("#button2"))); // true
+console.log(isDisabled(document.querySelector("#button3"))); // true
